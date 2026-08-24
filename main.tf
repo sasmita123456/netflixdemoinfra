eb08d2d6398ff5be57985373a6300609ad2c2845
sasmita123456 <121441536+sasmita123456@users.noreply.github.com>
2026-08-24 23:40:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,20 +5,22 @@ provider "aws" {
 resource "aws_instance" "one" {
   count                  = 3
   ami                    = "ami-0b6d9d3d33ba97d99"
-  instance_type          = "c7i-flex.large"
-  key_name               = "junks"
-  vpc_security_group_ids = ["sg-0a1fb8aa0587d50af"]
+  instance_type          = "t2.medium"
+  key_name               = "my-server"
+  subnet_id              = "subnet-0c7414134a4f95af1"
+
+  vpc_security_group_ids = ["sg-0e493b2a19a06e8ea"]
   tags = {
     Name = var.instance_names[count.index]
   }
 }
 
 variable "instance_names" {
-  default = ["jenkins", "tomcat-1", "Monitoring server"]
+  default = ["jenkins", "tomcat-1", "monitoring-server"]
 }
 
 resource "aws_s3_bucket" "one" {
-  bucket = "my-project-bucket-123456789"
+  bucket = "terraform-state-sasmita-001"
   versioning {
     enabled = true
   }
