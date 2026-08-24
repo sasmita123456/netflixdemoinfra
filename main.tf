a8d9ecf911f635620e47f465a431f1f62188cc27
sasmita123456 <121441536+sasmita123456@users.noreply.github.com>
2026-08-25 03:39:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,7 @@
-/* provider "aws" {
+provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "one" {
-  count                  = 3
-  ami                    = ""
-  instance_type          = ""
-  key_name               = ""
-  subnet_id              = ""
 
-  vpc_security_group_ids = [""]
-  tags = {
-    Name = var.instance_names[count.index]
-  }
-}
-
-variable "instance_names" {
-  default = ["jenkins", "tomcat-1", "monitoring-server"]
-} */
 
 
