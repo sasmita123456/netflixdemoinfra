f6ff2074314f50fdfdadec54301109c0871885bc
sasmita123456 <121441536+sasmita123456@users.noreply.github.com>
2026-08-24 23:20:17 +0530

    Add AWS provider and resources for instances and S3 bucket

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+  count                  = 3
+  ami                    = "ami-0b6d9d3d33ba97d99"
+  instance_type          = "c7i-flex.large"
+  key_name               = "junks"
+  vpc_security_group_ids = ["sg-0a1fb8aa0587d50af"]
+  tags = {
+    Name = var.instance_names[count.index]
+  }
+}
+
+variable "instance_names" {
+  default = ["jenkins", "tomcat-1", "Monitoring server"]
+}
+
+resource "aws_s3_bucket" "one" {
+  bucket = "my-project-bucket-123456789"
+  versioning {
+    enabled = true
+  }
+}
