206ead55d5ef34fe486e734b15f8d48ef3671bf8
sasmita123456 <121441536+sasmita123456@users.noreply.github.com>
2026-08-25 12:49:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 resource "aws_instance" "one" {
   count                  = 3
-  ami                    = "ami-0b6d9d3d33ba97d99"
-  instance_type          = "t2.medium"
-  key_name               = "my-server"
-  vpc_security_group_ids = ["sg-04a8c16be697b3873"]
+  ami                    = ""
+  instance_type          = ""
+  key_name               = ""
+  vpc_security_group_ids = [""]
   tags = {
     Name = var.instance_names[count.index]
   }
